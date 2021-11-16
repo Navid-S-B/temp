b55ab369accd6d3b63f13a71b02a46ca402b0e8f
Navid-S-B <navid.bhuiyan45@outlook.com.au>
2021-11-16 17:32:38 +1100

    removed semi-colon

--- client.py
+++ client.py
@@ -8,44 +8,46 @@
 from socket import *
 import sys
 
-#Server would be running on the same host as Client
-if len(sys.argv) != 3:
-    print("\n===== Error usage, python3 TCPClient3.py SERVER_IP SERVER_PORT ======\n");
-    exit(0);
-serverHost = sys.argv[1]
-serverPort = int(sys.argv[2])
-serverAddress = (serverHost, serverPort)
+if __name__ == "__main__":
 
-# define a socket for the client side, it would be used to communicate with the server
-clientSocket = socket(AF_INET, SOCK_STREAM)
+    #Server would be running on the same host as Client
+    if len(sys.argv) != 3:
+        print("\n===== Error usage, python3 TCPClient3.py SERVER_IP SERVER_PORT ======\n");
+        exit(0)
+    serverHost = sys.argv[1]
+    serverPort = int(sys.argv[2])
+    serverAddress = (serverHost, serverPort)
 
-# build connection with the server and send message to it
-clientSocket.connect(serverAddress)
+    # define a socket for the client side, it would be used to communicate with the server
+    clientSocket = socket(AF_INET, SOCK_STREAM)
 
-while True:
-    message = input("===== Please type any messsage you want to send to server: =====\n")
-    clientSocket.sendall(message.encode())
+    # build connection with the server and send message to it
+    clientSocket.connect(serverAddress)
 
-    # receive response from the server
-    # 1024 is a suggested packet size, you can specify it as 2048 or others
-    data = clientSocket.recv(1024)
-    receivedMessage = data.decode()
+    while True:
+        message = input("===== Please type any messsage you want to send to server: =====\n")
+        clientSocket.sendall(message.encode())
 
-    # parse the message received from server and take corresponding actions
-    if receivedMessage == "":
-        print("[recv] Message from server is empty!")
-    elif receivedMessage == "user credentials request":
-        print("[recv] You need to provide username and password to login")
-    elif receivedMessage == "download filename":
-        print("[recv] You need to provide the file name you want to download")
-    else:
-        print("[recv] Message makes no sense")
-        
-    ans = input('\nDo you want to continue(y/n) :')
-    if ans == 'y':
-        continue
-    else:
-        break
+        # receive response from the server
+        # 1024 is a suggested packet size, you can specify it as 2048 or others
+        data = clientSocket.recv(1024)
+        receivedMessage = data.decode()
 
-# close the socket
-clientSocket.close()
+        # parse the message received from server and take corresponding actions
+        if receivedMessage == "":
+            print("[recv] Message from server is empty!")
+        elif receivedMessage == "user credentials request":
+            print("[recv] You need to provide username and password to login")
+        elif receivedMessage == "download filename":
+            print("[recv] You need to provide the file name you want to download")
+        else:
+            print("[recv] Message makes no sense")
+            
+        ans = input('\nDo you want to continue(y/n) :')
+        if ans == 'y':
+            continue
+        else:
+            break
+
+    # close the socket
+    clientSocket.close()
